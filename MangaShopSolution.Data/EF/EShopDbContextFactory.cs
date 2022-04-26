121177b7fab7d32b8e86e072e2ef5cffb11cf0cb
VuQuan <VuQuan1997@gmail.com>
2022-04-26 10:23:47 +0700

    ...

---
+++ MangaShopSolution.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MangaShopSolution.Data.EF
+{
+    public class MangaShopDbContextFactory : IDesignTimeDbContextFactory<MangaShopDbContext>
+    {
+        public MangaShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("MangaShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<MangaShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new MangaShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
